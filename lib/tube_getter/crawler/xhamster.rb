899b42c79cd7276ada341ea1d4a61b503f7b567a
Rackcheckers <rackcheckers@gmail.com>
2014-03-15 20:14:15 +0100

    fixed xhamster crawler

--- lib/tube_getter/crawler/xhamster.rb
+++ lib/tube_getter/crawler/xhamster.rb
@@ -1,47 +1,17 @@
 module TubeGetter
   module Crawler
     class Xhamster < Base
-  
-      def initialize(url)
-        super
-        @agent.user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"
-      end
-  
+      
       def crawl
-        uri.subdomain = 'm'
-    
-        self.add_cookies()
-    
-        doc = self.get(uri.to_s)
+        doc = self.get(original_url)
     
         puts "\n" + (doc / 'title').inner_text + "\n\n"
     
-        a = doc.at('.noFlash/a.mp4Thumb')
+        a = doc.at('object/video/div.noFlash/a.mp4Thumb')
     
-        if a
-          puts a['href']
+        puts `wget -c -O "#{target_filename}" "#{a['href']}"`
+      end
       
-          puts "\nDownloading to #{target_filename}\n\n"
-      
-          puts `wget -c -O "#{target_filename}" "#{a['href']}"`
-        else
-          puts "Sorry, could not find link to MP4 file."
-        end
-      end
-  
-      def add_cookies
-        self.add_cookie("mobileUse", "0")
-        self.add_cookie("lang",      "en")
-        self.add_cookie("mobileUse", "Apple")
-      end
-  
-      def add_cookie(key, value)
-        cookie = Mechanize::Cookie.new(key, value)
-        cookie.domain = ".xhamster.com"
-        cookie.path = "/"
-        @agent.cookie_jar.add("http://xhamster.com", cookie)
-      end
-  
       # ------------------------------------------------------------------------------------------------------------
   
       def self.slug
