cb9609ab937bb93d34aced388bce807031ad28ab
Rackcheckers <rackcheckers@gmail.com>
2014-05-17 17:55:47 +0200

    added a crawler for youjizz

---
+++ lib/tube_getter/crawler/youjizz.rb
@@ -0,0 +1,42 @@
+module TubeGetter
+  module Crawler
+    class Youjizz < Base
+      
+      def crawl
+        doc = self.get(original_url)
+        
+        puts "\n" + (doc / 'title').inner_text + "\n\n"
+        
+        iframe_el = doc.at('td/iframe')
+        
+        iframe_url = iframe_el['src']
+        
+        iframe_doc = self.get(iframe_url)
+        
+        video_url = iframe_doc.body.gsub(/.*"file",encodeURIComponent\("([^"]+)"\).*/im, "\\1")
+        
+        puts video_url
+        
+        puts `wget -O "#{temp_filename}" "#{video_url}"`
+        
+        puts `#{TubeGetter::Config.ffmpeg_path} #{TubeGetter::Config.ffmpeg_default_options} -i "#{temp_filename}" -vcodec copy -acodec copy "#{target_filename}"`
+        
+        if File.exist?(target_filename) && File.size(target_filename) > 0
+          `rm "#{temp_filename}"`
+        end
+        
+      end
+      
+      # ------------------------------------------------------------------------------------------------------------
+  
+      def self.slug
+        "youjizz"
+      end
+  
+      def self.get_id_from_url(url)
+        url.gsub(/.*:.*-(\d+)\.html.*/, "\\1")
+      end
+      
+    end
+  end
+end
