a3979f7d267a9ce124d88048499021290b7a757b
Rackcheckers <rackcheckers@gmail.com>
2014-09-05 21:49:31 +0200

    add xnxx

---
+++ lib/tube_getter/crawler/xnxx.rb
@@ -0,0 +1,13 @@
+module TubeGetter
+  module Crawler
+    class Xnxx < YoutubeDlBase
+
+      # ------------------------------------------------------------------------------------------------------------
+
+      def self.slug
+        "xnxx"
+      end
+
+    end
+  end
+end
